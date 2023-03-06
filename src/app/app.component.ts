5d3d482f5043041f71039e8432bbcdb1e2f82b2b
Buğra Yüksel <109875603+BugraYukselMD@users.noreply.github.com>
2023-03-06 11:49:02 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { AfterContentChecked, Component } from '@angular/core';
 import { CookieService_ } from 'src/services/cookie.service';
+import { Blog } from 'src/models/modelBlog';
+import { BlogRepository } from 'src/repositories/blog.repository';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,15 @@ import { CookieService_ } from 'src/services/cookie.service';
 })
 export class AppComponent implements AfterContentChecked {
   title = 'blog--frontend';
+  blogs:Blog[] = [];
 
-  constructor(private cookieService: CookieService_) {}
+  constructor(private 
+    cookieService: CookieService_,
+    private blogRepo: BlogRepository
+  ) {}
 
   ngAfterContentChecked(): void {
+    this.blogs = this.blogRepo.getBlogs();
     var token = this.cookieService.pureToken;
     if (token) {
       if (this.cookieService.isTokenExpired()) {
